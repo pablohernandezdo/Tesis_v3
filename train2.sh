7385e85c19aa6e28f0022812f2a2b7440821e3ac
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-04-22 04:00:30 -0400

    Add train only STEAD file

---
+++ train2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python train.py \
+        --lr 1e-4 \
+        --epochs 1 \
+        --batch_size 256 \
+        --patience 30 \
+        --model_folder 'models'  \
+        --classifier Cnn1_3k_10 \
+        --model_name Cnn1_3k_10_1e4_256_30 \
+        --train_path "Data_HDF5/STEAD_STEAD_0.8_train.hdf5" \
+        --val_path "Data_HDF5/STEAD_STEAD_0.1_val.hdf5"
