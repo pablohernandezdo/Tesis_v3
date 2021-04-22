f3a45dcebbcf405fff083acee08a2777829a1a61
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-04-22 19:25:03 -0400

    Test npy training speed

---
+++ train_npy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python train.py \
+        --lr 1e-4 \
+        --epochs 1 \
+        --batch_size 256 \
+        --patience 30 \
+        --model_folder 'models'  \
+        --classifier Cnn1_3k_10 \
+        --model_name Cnn1_3k_10_1e4_256_30_SG \
+        --train_path "Data_HDF5/STEAD_small_0.8_train.npy" \
+        --val_path "Data_HDF5/STEAD_small_0.1_val.npy"
